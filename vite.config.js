c3ca23f3b503747f4ca33b4dd356c2f2e29bf621
Matheus Polanczyk <matheus.polanczyk@hotmail.com>
2026-04-16 11:42:34 +0100

    fix: ajuste de base path e vercel config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/olhaoproduto/',
+  base: './', // Isso garante que os caminhos dos arquivos fiquem corretos no deploy
 })
