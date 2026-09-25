f44dc6665be679e61aaa3994c4d82d9cb0a923b5
Matheus Polanczyk <matheus.polanczyk@hotmail.com>
2026-09-25 21:45:21 +0100

    Add copyright footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,12 @@ import Home from './pages/Home'
 
 function App() {
   return (
-    <Home />
+    <>
+      <Home />
+      <footer style={{ padding: '18px 16px', color: '#9aa4bd', textAlign: 'center', fontSize: 13 }}>
+        © 2026 Matheus Polanczyk. Todos os direitos reservados.
+      </footer>
+    </>
   )
 }
 
